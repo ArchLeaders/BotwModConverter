443ffb7e2b6f6c141ec2d2a33f4e8cc5eb89557b
Cod3xDev <67140619+Cod3xDev@users.noreply.github.com>
2022-11-05 14:13:12 +1000

    Basic .NET MAUI GUI

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace BotwConverter;
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void onBnpButtonPressed(object sender, EventArgs e)
+	{
+        var customFileType = new FilePickerFileType(
+                    new Dictionary<DevicePlatform, IEnumerable<string>>
+                    {
+                        { DevicePlatform.WinUI, new[] { ".bnp" } },
+                        { DevicePlatform.macOS, new[] {"bnp"} }
+                    });
+
+        PickOptions options = new()
+        {
+            PickerTitle = "Please select a bnp file",
+            FileTypes = customFileType
+        };
+        FilePicker.Default.PickAsync(options);
+    }
+}
+
