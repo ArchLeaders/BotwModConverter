8e4778fbeddbbf4cedb21310053a7599e3b57cc7
Cod3xDev <67140619+Cod3xDev@users.noreply.github.com>
2022-11-05 18:39:27 +1000

    Copy MAUI into repo

---
+++ MauiProgram.cs
@@ -0,0 +1,18 @@
+﻿namespace BotwConverter;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("HyliaSerifBeta-Regular.otf", "hylia");
+
+			});
+
+		return builder.Build();
+	}
+}
