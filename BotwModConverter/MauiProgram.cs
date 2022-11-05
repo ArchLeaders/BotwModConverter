b8acea1862444c7279d8b3f0f40e026d9024c7be
ArchLeaders <archparliament@gmail.com>
2022-11-04 22:30:11 -0700

    Fix project layout

---
+++ BotwModConverter/MauiProgram.cs
@@ -0,0 +1,17 @@
+﻿namespace BotwConverter;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("HyliaSerifBeta-Regular.otf", "hylia");
+			});
+
+		return builder.Build();
+	}
+}
