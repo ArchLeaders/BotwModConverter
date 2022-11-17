5927f4d9e2ae7a1ffe1b72ab80eed63da130012d
ArchLeaders <archparliament@gmail.com>
2022-11-17 00:11:35 -0800

    Create generic IBotwConverter interface

---
+++ BotwModConverter.Core/IBotwConverter.cs
@@ -0,0 +1,8 @@
+﻿namespace BotwModConverter.Core
+{
+    internal interface IBotwConverter
+    {
+        public byte[] ConvertToWiiu(byte[] data);
+        public byte[] ConvertToSwitch(byte[] data);
+    }
+}
