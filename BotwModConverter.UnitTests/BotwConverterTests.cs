4302b3045a8a5ed04b7129bf122a702ace4e1fe7
ArchLeaders <archparliament@gmail.com>
2022-11-17 00:17:59 -0800

    Add Console and UnitTesting projects

---
+++ BotwModConverter.UnitTests/BotwConverterTests.cs
@@ -0,0 +1,20 @@
+﻿using BotwModConverter.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwModConverter.UnitTests
+{
+    [TestClass]
+    public class BotwConverterTests
+    {
+        [TestMethod]
+        [DataRow(@"D:\Botw\Mods\Works in Progress\Bunker\Bunker\Build")]
+        public async Task Convert(string mod)
+        {
+            await BotwConverter.Convert(mod);
+        }
+    }
+}
