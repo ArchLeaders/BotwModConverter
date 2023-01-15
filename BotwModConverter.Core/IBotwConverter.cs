3a43186cc46ba89960c4c5f0abd2e9276685566c
ArchLeaders <archparliament@gmail.com>
2023-01-15 01:05:23 -0800

    Use `Span<byte>` for data transfer

--- BotwModConverter.Core/IBotwConverter.cs
+++ BotwModConverter.Core/IBotwConverter.cs
@@ -2,7 +2,7 @@
 {
     public interface IBotwConverter
     {
-        public Task<byte[]> ConvertToWiiu(byte[] data);
-        public Task<byte[]> ConvertToSwitch(byte[] data);
+        public Span<byte> ConvertToWiiu(ReadOnlySpan<byte> data);
+        public Span<byte> ConvertToSwitch(ReadOnlySpan<byte> data);
     }
 }
