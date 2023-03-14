22fa3bc948c6a8a9e1bd671af6c51c98465317e2
ArchLeaders <archparliament@gmail.com>
2023-03-14 14:59:25 -0700

    Remove legacy converters

--- BotwModConverter.Core/LegacyConverters/BARS.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BotwModConverter.Core.LegacyConverters;
-
-public class BARS
-{
-    public static async Task<byte[]> Convert(byte[] data)
-    {
-        return new byte[0];
-    }
-}
--- BotwModConverter.Core/LegacyConverters/BFLIM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BotwModConverter.Core.LegacyConverters;
-
-public class BFLIM
-{
-    public static async Task<byte[]> Convert(byte[] data)
-    {
-        throw new NotImplementedException();
-    }
-}
