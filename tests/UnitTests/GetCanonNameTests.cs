d43c203bc252ec2dcfaa4180af6cd7a2b57fc71f
ArchLeaders <archleadership28@gmail.com>
2026-02-11 17:35:23 -0800

    Add unit tests for GetCanonName

---
+++ tests/UnitTests/GetCanonNameTests.cs
@@ -0,0 +1,17 @@
+using FluentAssertions;
+using BotwModConverter.Core.Utils;
+
+namespace UnitTests;
+
+public class GetCanonNameTests
+{
+    [Theory]
+    [InlineData(".sr", ".r")]
+    [InlineData("Test.sbarc", "Test.barc")]
+    [InlineData("Test.sarc", "Test.sarc")]
+    [InlineData("F:\\Some\\Path\\And\\File.ssarc", "File.sarc")]
+    public static void ShouldStripSPrefix(string source, string result)
+    {
+        ModHelper.GetCanonName(source).ToString().Should().Be(result);
+    }
+}
